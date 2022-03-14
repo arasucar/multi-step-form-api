7a51b756461dcb3d800a98ecc7d847c2c9730ac4
Aras <a.arasucar@gmail.com>
2022-03-14 20:41:16 +0100

    Forgotten diffs have been added.

--- src/kotlin/com/arasucar/multipageformapi/controller/FormController.kt
+++ src/kotlin/com/arasucar/multipageformapi/controller/FormController.kt
@@ -1,24 +1,19 @@
 package com.arasucar.multipageformapi.controller
 
 import com.arasucar.multipageformapi.model.Form
+import com.arasucar.multipageformapi.service.FormService
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 
 @RestController
-class FormController {
-
-    var form: Form? = null;
+class FormController(val formService: FormService) {
 
     @GetMapping(value = ["/form"], produces = [MediaType.APPLICATION_JSON_VALUE])
     @CrossOrigin(origins = ["http://localhost:3000"])
-    fun getFormFromMemory(): Form? {
-        return form
-    }
+    fun getFormFromMemory(): Form? = formService.getFormObject()
 
     @PostMapping(value = ["/form"], consumes = [MediaType.APPLICATION_JSON_VALUE])
     @CrossOrigin(origins = ["http://localhost:3000"])
-    fun saveForm(@RequestBody form: Form) {
-        this.form = form
-    }
+    fun saveForm(@RequestBody form: Form) = formService.saveForm(form)
 
 }
--- src/kotlin/com/arasucar/multipageformapi/service/FormService.kt
+++ src/kotlin/com/arasucar/multipageformapi/service/FormService.kt
@@ -1,4 +1,19 @@
 package com.arasucar.multipageformapi.service
 
+import com.arasucar.multipageformapi.model.Form
+import org.springframework.stereotype.Service
+
+@Service
 class FormService {
+
+    var form: Form? = null;
+
+    fun getFormObject(): Form? {
+        return this.form;
+    }
+
+    fun saveForm(form: Form) {
+        this.form = form
+    }
+
 }
